a6d9b422f26fde97eb350d064a904ecbf78bc2f4
Cian Cronin <ccroninpersonal@gmail.com>
2020-12-13 02:43:45 +0000

    Day 3 Soln
    Advent of Code Day 3 Solution

--- 03_day/main.cpp
+++ 03_day/main.cpp
@@ -3,24 +3,46 @@
 #include <fstream>
 #include <vector>
 
-void readData(std::vector<std::string> &data) {
+void answerQuestions() {
 
     std::string inFileName = "data.txt";
     std::ifstream inFile;
     inFile.open(inFileName);
     std::string str;
 
+    int noOfTrees[] = {0, 0, 0, 0, 0};
+
     if(inFile.is_open()) {
 
-        while(getline(inFile, str)) {
-            // Do Something
+        int hPos = 0;
+        while(std::getline(inFile, str)) {
+
+            if (str.empty()) break;
+            int lineLength = str.length();
+
+            // If the hPos is a tree, increase noOfTree count
+            if (str[hPos % lineLength] == '#') noOfTrees[0]++;
+            if (str[(hPos * 3) % lineLength] == '#') noOfTrees[1]++;
+            if (str[(hPos * 5) % lineLength] == '#') noOfTrees[2]++;
+            if (str[(hPos * 7) % lineLength] == '#') noOfTrees[3]++;
+            if (str[(hPos / 2) % lineLength] == '#' && hPos % 2 == 0) noOfTrees[4]++; // Divide by 2 due to 2 hPos skip
+
+            // Move the next hPos trackers over
+            hPos++;
         }
     } else {
         std::cerr << "Data file not found." << std::endl;
     }
 
+    int64_t product{1}; // Overflow on an unsigned int
+    for (int i : noOfTrees) {std::cout << i << "\n"; product *= i;}
+
+    std::cout << "Product: " << product << std::endl;
+
 }
 
 int main() {
-    // open squares (.) and trees (#)
+    answerQuestions();
+
+    return 0;
 }
